071e00c9a42f9de92ce5f8580201888cd88e7d1b
Jack Cho <jack06215@gmail.com>
2022-05-05 17:59:40 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_practices/pages/counter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter_Riverpod_Practices',
+      theme: ThemeData(
+        canvasColor: Colors.indigo.withBlue(2),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.lightBlue.withBlue(3)),
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CounterPage(),
+    );
+  }
+}
---
+++ lib/pages/counter.dart
@@ -0,0 +1,86 @@
+import 'package:flutter_riverpod_practices/riverpod_counter.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final appCounterProvider = ChangeNotifierProvider<Counter>((ref) {
+  return Counter();
+});
+
+class CounterPage extends HookConsumerWidget {
+  const CounterPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final mq = MediaQuery.of(context).size;
+    final applyCount = ref.watch(appCounterProvider);
+    final appName = ref.watch(appNameProvider);
+    return SafeArea(
+      child: Scaffold(
+        appBar: _appBar(appName, context),
+        floatingActionButton: rowfloatingActionButton(applyCount, mq),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                "Value: ${applyCount.value.toString()}",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6!
+                    .copyWith(color: Colors.white),
+              ),
+            ),
+            SizedBox(
+              height: mq.height * 0.02,
+            ),
+            Text(
+              "Riverpod Counter Example",
+              style: Theme.of(context)
+                  .textTheme
+                  .caption!
+                  .copyWith(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  AppBar _appBar(String appName, BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      centerTitle: true,
+      title: Text(
+        appName,
+        style: Theme.of(context)
+            .textTheme
+            .headline5!
+            .copyWith(color: Colors.white),
+      ),
+    );
+  }
+
+  Widget rowfloatingActionButton(Counter applyCount, Size mq) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+          onPressed: () {
+            applyCount.add();
+          },
+          child: const Icon(Icons.add),
+        ),
+        SizedBox(
+          width: mq.width * 0.02,
+        ),
+        FloatingActionButton(
+          onPressed: () {
+            applyCount.subtract();
+          },
+          child: const Icon(Icons.remove),
+        ),
+      ],
+    );
+  }
+}
