ebe64400631372eaf3a912cab096d5c52359fd00
Jack Cho <jack06215@gmail.com>
2022-05-18 23:04:46 +0800

    compare rebuild count

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// This example showcases how extracting widgets into StatelessWidgets
+// instead of functions can improve performances, by rebuilding
+// only what needs to update when the state changes
 
-final firstCounter = ValueNotifier(0);
-final secondCounter = ValueNotifier(0);
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home(),
+  Widget build(context) {
+    return Counter(
+      count: Count(),
+      child: MaterialApp(
+        home: Home(),
+      ),
     );
+  }
+}
+
+class Count extends ValueNotifier<int> {
+  Count() : super(0);
+}
+
+class Counter extends InheritedNotifier {
+  const Counter({
+    Key? key,
+    required this.count,
+    required Widget child,
+  }) : super(key: key, child: child, notifier: count);
+
+  final Count count;
+
+  static Count of(BuildContext context, {bool listen = true}) {
+    if (listen) {
+      return context.dependOnInheritedWidgetOfExactType<Counter>()!.count;
+    } else {
+      final Counter counter = context
+          .getElementForInheritedWidgetOfExactType<Counter>()!
+          .widget as Counter;
+      return counter.count;
+    }
   }
 }
 
@@ -23,76 +50,33 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print('Home build');
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ValueListenableBuilder<int>(
-              valueListenable: firstCounter,
-              builder: (context, value, child) {
-                return TweenAnimationBuilder<int>(
-                  duration: const Duration(seconds: 5),
-                  curve: Curves.easeOut,
-                  tween: IntTween(end: value),
-                  builder: (context, value, child) {
-                    return Text('$value');
-                  },
-                );
-              },
-            ),
-            ElevatedButton(
-              onPressed: () => firstCounter.value += 100,
-              child: const Text('+'),
-            ),
-            // Both counters have voluntarily a different Curve and duration
-            ValueListenableBuilder<int>(
-              valueListenable: secondCounter,
-              builder: (context, value, child) {
-                return TweenAnimationBuilder<int>(
-                  duration: const Duration(seconds: 2),
-                  curve: Curves.easeInOut,
-                  tween: IntTween(end: value),
-                  builder: (context, value, child) {
-                    return Text('$value');
-                  },
-                );
-              },
-            ),
-            ElevatedButton(
-              onPressed: () => secondCounter.value += 100,
-              child: const Text('+'),
-            ),
-            const Text('total:'),
-            // The total must take into account the animation of both counters
-            ValueListenableBuilder<int>(
-              valueListenable: firstCounter,
-              builder: (context, value, child) {
-                return TweenAnimationBuilder<int>(
-                  duration: const Duration(seconds: 5),
-                  curve: Curves.easeOut,
-                  tween: IntTween(end: value),
-                  builder: (context, value, child) {
-                    return ValueListenableBuilder<int>(
-                      valueListenable: secondCounter,
-                      builder: (context, value2, child) {
-                        return TweenAnimationBuilder<int>(
-                          duration: const Duration(seconds: 2),
-                          curve: Curves.easeInOut,
-                          tween: IntTween(end: value2),
-                          builder: (context, value2, child) {
-                            return Text('${value + value2}');
-                          },
-                        );
-                      },
-                    );
-                  },
-                );
-              },
-            ),
-          ],
-        ),
+        // By extracting Title in a StatelessWidget, Home doesn't rebuild when the counter changes
+        child: Title(),
+        // Uncommenting this code causes Home to rebuild when the counter changes
+        // child: title(context),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Counter.of(context, listen: false).value++,
+        child: const Icon(Icons.add),
       ),
     );
   }
+}
+
+class Title extends StatelessWidget {
+  const Title({Key? key}) : super(key: key);
+
+  @override
+  Widget build(context) {
+    print('build Title');
+    return Text('Count ${Counter.of(context).value}');
+  }
+}
+
+Widget title(context) {
+  print('build Title');
+  return Text('Count ${Counter.of(context).value}');
 }
