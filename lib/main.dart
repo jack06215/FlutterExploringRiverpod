07f7053f12ed2a5a8cf468ba669693ecd94016f4
Jack Cho <jack06215@gmail.com>
2022-05-29 14:21:12 +0800

    reference

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod_practices/views/home_page.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+// https://github.com/Maxvyr/adopt_ta_waifu_android_flutter/blob/main/lib/repository/call_api.dart
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
